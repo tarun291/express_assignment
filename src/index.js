a747510a99a155a7754790c1d1f6d97082d40c15
tarun291 <tarunkumar222020@gmail.com>
2023-08-26 07:35:44 +0530

    Db setup completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
+const sequelize = require('./db/index')
 
 const app = express();
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(3000, () => {
+    sequelize.sync({ force: true }).then(() => {
+        console.log('woo');
+    })
     console.log('app is running');
 })
